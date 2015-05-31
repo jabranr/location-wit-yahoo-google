e015458e27d32b01d32ff98e14986f388043cc14
Jabran Rafique <hello@jabran.me>
2015-06-01 00:02:38 +0100

    finalize methods

--- js/main.js
+++ js/main.js
@@ -17,60 +17,177 @@
 		return select + where + and;
 	}
 
-	function extractLocationData(data) {
-		if ( data && data.match ) {
-			var matches = data.match;
+	function render(input) {
+		return $('#output')
+			.empty()
+			.html(JSON.stringify(input, null, 4))
+			.wrap( $('<pre />', { 'class': 'error' }) );
+	}
 
-			if ( matches.length > 0 ) {
-				for (var i = 0; i < matches.length; i++) {
-					var match = matches[i];
-					if (match.place) {
-						app.foundLocations.push(match.place);
-					}
-				}
-			}
-			else {
-				if (matches.place) {
-					app.foundLocations.push(matches.place);
-				}
-			}
-		}
+	function renderAppend(input) {
+		var $output = $('#output');
+		var $html = $output.html();
+		return $output
+			.html($html + '\r\n' + JSON.stringify(input, null, 4));
 	}
 
 	var app = {
-		foundLocations: [],
+		foundLocations: {},
+		errorLog: [],
 		yahooApi: 'https://query.yahooapis.com/v1/public/yql',
+		witAppId: '5555fe3b-a58f-44cc-8689-92daec576492',
+		witToken: 'EEOSBAWMRZVQUMEIKCBAR2V6UWLNMWBW',
+		witApi: 'https://api.wit.ai/message',
+		witVersion: '20150515',
+
+		extractLocationData: function(data) {
+			var self = app;
+
+			// result from yahoo
+			if ( data.query ) {
+				if ( data.query.results && data.query.results.matches && data.query.results.matches.match ) {
+					var matches = data.query.results.matches.match;
+
+					if ( matches.length > 0 ) {
+						for (var i = 0; i < matches.length; i++) {
+							var match = matches[i];
+							if (match.place) {
+								self.foundLocations.yahoo = match.place;
+							}
+						}
+					}
+					else {
+						if (matches.place) {
+							self.foundLocations.yahoo = matches.place;
+						}
+					}
+				}
+				else {
+					self.errorLog.push({
+						yahoo: data
+					});
+				}
+			}
+
+			// result from wit
+			if ( data.msg_id ) {
+				if ( data.outcomes ) {
+
+					var matches = data.outcomes;
+
+					for (var i = 0; i < matches.length; i++) {
+						var match = matches[i];
+						if ( match.intent && match.intent === 'location' ) {
+							if (match.entities && match.entities.location) {
+								self.foundLocations.wit = match.entities.location;
+							}
+							else {
+								self.errorLog.push({
+									wit: data
+								});
+							}
+						}
+					}
+
+					self.googleLocationData();
+				}
+				else {
+					self.errorLog.push({
+						wit: data
+					});
+				}
+			}
+		},
+
+		googleLocationData: function() {
+			if ( ! app.foundLocations ) return;
+			var data = app.foundLocations;
+			var wit;
+
+			for (var key in data) {
+				if ( data.hasOwnProperty(key) && key === 'wit' ) {
+					wit = data[key];
+				}
+			}
+
+			if ( wit ) {
+				wit.map(app.geocodeWitLocation);
+			}
+		},
+
+		geocodeWitLocation: function(data) {
+			if ( ! data || ! data.value ) return;
+			if ( ! typeof google === 'undefined' )
+				throw new Error('Google Maps API SDK are not loaded.');
+
+			app.foundLocations.google = app.foundLocations.google || [];
+			var geocoder = new google.maps.Geocoder();
+			geocoder.geocode({
+				address: data.value
+			}, function(results, status) {
+				if (status === google.maps.GeocoderStatus.OK) {
+					var result = results[0];
+
+					app.foundLocations.google.push({
+						_data: result,
+						address: result.formatted_address,
+						accuracy: result.partial_match ? 'partial' : 'exact',
+						position: {
+							lat: result.geometry.location.lat(),
+							lng: result.geometry.location.lng()
+						}
+					});
+
+					render(app.foundLocations);
+				}
+			});
+		},
 
 		init: function(query, output, submit) {
 			if ( ! query.val().length ) return query.focus();
-			var that = this;
+			this.query = query;
+			var self = this;
 
-			$.ajax({
-			  url: that.yahooApi,
-			  data: {
-			    'q': makeQuery(query.val()),
-			    'format': 'json'
-			  },
-			  method: 'GET',
-			  success: function(data) {
-			  	if (data) {
+			$.when(
+				$.ajax({
+					url: self.yahooApi,
+					method: 'GET',
+					data: {
+						q: makeQuery(self.query.val()),
+						format: 'json'
+					}
+				}),
+				$.ajax({
+					url: self.witApi,
+					method: 'GET',
+					dataType: 'jsonp',
+					data: {
+						q: self.query.val(),
+						v: self.witVersion,
+						access_token: self.witToken
+					}
+				})
+			).then(
+				function(yahoo, wit) {
+					self.extractLocationData(yahoo[0]);
+					self.extractLocationData(wit[0]);
 
-			  		if ( data.query && data.query.results && data.query.results.matches ) {
-				  		extractLocationData(data.query.results.matches);
-			  		}
+					if ( self.errorLog.length ) {
+						render(self.errorLog);
+					}
 
-		  			output.html(JSON.stringify(data, null, 4))
-		  				.wrap( $('<pre />') );
-		  		}
-			  },
-			  error: function(xhr, error) {
-			  	output
-			  		.html(JSON.stringify(xhr.responseJSON), null, 4)
-			  		.wrap( $('<pre />', {
-				  		'class': 'error'
-				  	}) );
-			  }
-			});
+					if ( self.foundLocations ) {
+						render(self.foundLocations);
+					}
+				},
+				function(xhr, error) {
+					render(error);
+				}
+			);
+		},
+
+		isArray: function(obj) {
+			return (Object.prototype.toString.call(obj) === '[objcet Array]');
 		}
 
 	};
